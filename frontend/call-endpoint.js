65743a5d3268acdd82ff47c532528496815f657a
tara-matthew <taramatthew.93@gmail.com>
2025-07-28 18:19:35 +0100

    Add script to call endpoint

---
+++ frontend/call-endpoint.js
@@ -0,0 +1,44 @@
+import fetch from 'node-fetch';
+import {exec} from "node:child_process";
+
+async function callAddToBasket() {
+    const url = `${process.env.API_URL}/basket`;
+    const payload = {
+        email: process.env.EMAIL,
+        password: process.env.PASSWORD,
+        product_names: ["oatcakes", "salmon"],
+    };
+
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(payload),
+        });
+
+        console.log(response)
+
+        if (!response.ok) {
+            console.error('Error:', response.status, response.statusText);
+            const errorText = await response.text();
+            console.error('Details:', errorText);
+            return;
+        }
+
+        const ids = await response.json();
+        console.log('Response from server:', result);
+        exec(`node ./run-playwright.js '${ids}'`, (error, stdout, stderr) => {
+            if (error) {
+                console.error('Error running Playwright:', error);
+            } else {
+                console.log('Playwright output:', stdout);
+            }
+        });
+    } catch (err) {
+        console.error('Fetch error:', err);
+    }
+}
+
+await callAddToBasket();
