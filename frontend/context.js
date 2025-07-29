94bbcbcd45711ac1fdb21fca36b7f00a80ac1dfa
tara-matthew <taramatthew.93@gmail.com>
2025-07-29 17:33:07 +0100

    Update context logic

---
+++ frontend/context.js
@@ -0,0 +1,9 @@
+
+export async function createContext(browser) {
+    return await browser.newContext({
+        userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64)...",
+        viewport: { width: 1280, height: 720 },
+        locale: "en-UK",
+        timezoneId: "Europe/London",
+    });
+}
--- frontend/run-playwright.js
+++ frontend/run-playwright.js
@@ -1,11 +1,11 @@
-import { chromium } from "playwright";
 import fs from "fs";
-import path from "path";
 import dotenv from "dotenv";
 import { login } from "./auth.js";
 import { addToBasket } from "./basket.js";
 import { HOME_URL, COOKIES_PATH } from "./constants.js";
 import {dismissCookieBanner, loadCookies, saveCookies} from "./cookies.js";
+import {createContext} from "./context.js";
+import {chromium} from "playwright";
 
 dotenv.config();
 
@@ -13,19 +13,14 @@ const args = JSON.parse(process.argv[2]);
 
 (async () => {
     const browser = await chromium.launch({ headless: false, slowMo: 300 });
-    const context = await browser.newContext({
-        userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64)...",
-        viewport: { width: 1280, height: 720 },
-        locale: "en-UK",
-        timezoneId: "Europe/London",
-    });
-
-    const page = await context.newPage();
-    const pageContext = page.context()
+    const context = await createContext(browser)
 
     if (fs.existsSync(COOKIES_PATH)) {
         await loadCookies(context)
     }
+
+    const page = await context.newPage();
+    const pageContext = page.context()
 
     try {
         await page.goto(HOME_URL);
