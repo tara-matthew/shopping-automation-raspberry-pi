a55652df71f06b205c024d252f08a043592284ef
tara-matthew <taramatthew.93@gmail.com>
2025-07-29 17:42:22 +0100

    Add linting plugins, set up config

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,21 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import stylistic from '@stylistic/eslint-plugin'
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, '@stylistic': stylistic, "unused-imports": unusedImports, },
+    rules: {
+      "no-unused-vars": "error",
+      "unused-imports/no-unused-imports": "error",
+      "no-undef": "error",
+      "@stylistic/object-curly-spacing": ["error", "always"],
+      "@stylistic/jsx-curly-spacing": ["error", "always"],
+      "@stylistic/brace-style": ["error", "1tbs"],
+
+    },
+    languageOptions: { globals: globals.browser }
+  },
 ]);
